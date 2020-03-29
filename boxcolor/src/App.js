e5f8bc08d297d3baaa00819330527f163e356323
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-03-29 01:41:42 -0400

    added more code to App.js/css & index.css

--- boxcolor/src/App.js
+++ boxcolor/src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+// Pseudocode:
+// - Update functional component to a class component (App.js)
+// - Set a state (hint: color & clickCounter)
+// - When the button is clicked it should change its color and the    counter should increase.
+// - Use a component lifecycle method to call setState.
 
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: 'blue'
+    }
+  }
+
+  onChange = (event) => {
+    this.setState({
+      color: 'orange'
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Box color change</h1>
+
+        <div
+          className="colorBox"
+          style={{ backgroundColor: this.state.color }}
+          onClick={this.onChange}
+        >
+          <p>Click Me!</p>
+        </div>
+      </div >
+    )
+  }
 }
 
 export default App;
+
